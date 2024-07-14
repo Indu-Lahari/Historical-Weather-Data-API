cbf9abc07c1815163cf24852c3d8bbf66ee0d6d8
Indu <indulahari6@gmail.com>
2024-07-14 18:27:25 +0530

    Show stations data table on web page

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import pandas
 
 app = Flask(__name__)
 
+stations = pandas.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/<station>/<date>")
